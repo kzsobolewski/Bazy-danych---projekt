4492c8c88a3e778e9f50d40f135a2139c83b0795
kzsobolewski <kzsobolewski@gmail.com>
2018-05-27 22:29:31 +0200

    first commit - hello world

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(2000, () => console.log('Example app listening on port 2000!'))
