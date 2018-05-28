3bde74b009d45fa06e1f46b3983f55b003939c5e
kzsobolewski <kzsobolewski@gmail.com>
2018-05-28 03:01:53 +0200

    added mysql connection, nodemon and basic 3 pages

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
-const express = require('express')
-const app = express()
+// Modules
+const express = require('express');
+const mysql = require('mysql');
+const path = require('path'); // path is installed with node.js | DON'T USE 'npm i path'
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// Initializing express
+const app = express();
 
-app.listen(2000, () => console.log('Example app listening on port 2000!'))
+// Mysql connection
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'admin',
+  password: 'secret',
+  database: 'myDB'
+});
+
+connection.connect(err => {
+  if (err)
+    console.error('[MySql] ' + err);
+  else
+    console.log('[MySql] Connected');
+});
+
+
+// Paths
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/admin', (req, res) => {
+  // Rendering STATIC html file
+  // path.join() creates absolute path to file connecting __dirname
+  // (path to dir (?)) with path RELATIVE to index.js (this file)
+  res.sendFile(path.join(__dirname, './public/admin.html'));
+});
+
+app.get('/worker', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/worker.html'))
+});
+
+
+var port = 3306 + 12;
+app.listen(port, function() {
+  console.log('SZCPP is listening on port', port);
+});
+
+// public is dir with our page (frontend)
+// everything outside is server (backend)
